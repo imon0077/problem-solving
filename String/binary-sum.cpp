e886e794dae7525abe7e822694a530b740f538b1
Imon Islam <imon0077@yahoo.com>
2023-08-14 15:13:05 +0600

    binary sum

--- String/binary-sum.cpp
+++ String/binary-sum.cpp
@@ -1,16 +1,53 @@
 #include<iostream>
+#include<algorithm>
+#include<vector>
 
 using namespace std;
 
 int main(){
-    string a = "11";
-    int aLen = a.size();
+    string a = "11", b = "10", result = "";
 
-    for (int i = 0; i < aLen; i++)
+    reverse(a.begin(), a.end()); 
+    reverse(b.begin(), b.end());
+
+    int aLen = a.size(), bLen = b.size(), carry = 0, i = 0, sum;
+
+    while (i < aLen || i < bLen)
     {
-        cout << "\n " << a[i] - '0';
+        sum = carry;
+        if(i < aLen)
+            sum = sum + (a[i] - '0');
+        if(i < bLen)
+            sum = sum + (b[i] - '0');
+
+
+        if(sum == 0){
+            carry = 0; 
+            result += '0';
+        }
+        else if(sum == 1){
+            carry = 0; 
+            result += '1';
+        }
+        else if(sum == 2){
+            carry = 1; 
+            result += '0';
+        }
+        else{
+            carry = 1; 
+            result += '1';
+        }
+
+        i++;
+    }
+
+    if(carry == 1){
+        result += '1';
     }
     
+    reverse(result.begin(), result.end());
+
+    cout << "\n " << result;
 
     return 0;
 }
